da03bc084010d7dcb692fb8495754f0c3d7adf7d
PriyanshuKumar <priyanshurazz4@gmail.com>
2025-08-20 20:59:02 +0530

    Api Url changed

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/users/";
+const API_URL = "https://firstfullstackserver.onrender.com/api/users/";
 
 // Register user
 const register = async (userData) => {
